5f4019d2f4641cbe242d2a3cf36f534e7c60e828
Alexr <100096112+dskjfoisjfsjio@users.noreply.github.com>
2025-02-26 23:23:28 -0500

    Update openai.js

--- openai.js
+++ openai.js
@@ -12,7 +12,7 @@ const openai = new OpenAI({
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
